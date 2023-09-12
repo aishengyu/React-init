293f048bbf615315a88625bfbe6df95e399996a8
aishengyu <134182371+aishengyu@users.noreply.github.com>
2023-09-12 14:53:48 +0900

    １．最初のHTML構造を用意する

--- src/App.jsx
+++ src/App.jsx
@@ -2,5 +2,38 @@ import React from "react";
 import "./styles.css";
 
 export const App = () => {
-  return <div>init</div>;
+  return (
+    <>
+      <div>
+        <input placeholder="TODOを入力" />
+        <button>追加</button>
+      </div>
+      <div>
+        <ul>
+          <div>
+            <li>ああああ</li>
+            <button>完成</button>
+            <button>削除</button>
+          </div>
+          <div>
+            <li>ああああ</li>
+            <button>完成</button>
+            <button>削除</button>
+          </div>
+        </ul>
+      </div>
+      <div>
+        <ul>
+          <div>
+            <li>うううう</li>
+            <button>戻す</button>
+          </div>
+          <div>
+            <li>おおお</li>
+            <button>戻す</button>
+          </div>
+        </ul>
+      </div>
+    </>
+  );
 };
