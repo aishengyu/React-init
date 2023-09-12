e8a3463e8a5b979ee15a5f3276c0cda09ed9abc0
aishengyu <134182371+aishengyu@users.noreply.github.com>
2023-09-12 14:58:34 +0900

    2.CSSを適用する

--- src/App.jsx
+++ src/App.jsx
@@ -4,31 +4,31 @@ import "./styles.css";
 export const App = () => {
   return (
     <>
-      <div>
+      <div class="input-area ">
         <input placeholder="TODOを入力" />
         <button>追加</button>
       </div>
-      <div>
+      <div class="imcomplete-area">
         <ul>
-          <div>
+          <div class="list-row">
             <li>ああああ</li>
             <button>完成</button>
             <button>削除</button>
           </div>
-          <div>
+          <div class="list-row">
             <li>ああああ</li>
             <button>完成</button>
             <button>削除</button>
           </div>
         </ul>
       </div>
-      <div>
+      <div class="complete-area">
         <ul>
-          <div>
+          <div class="list-row">
             <li>うううう</li>
             <button>戻す</button>
           </div>
-          <div>
+          <div class="list-row">
             <li>おおお</li>
             <button>戻す</button>
           </div>
