f7f20f5e08e7d2d5a29253fdac965a73df4014b0
aishengyu <134182371+aishengyu@users.noreply.github.com>
2023-09-12 15:16:34 +0900

    4.べた書きを可変に変更する

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,12 @@
-import React from "react";
+import React, { useState } from "react";
 import "./styles.css";
 
 export const App = () => {
+  const [imcompleteTodo, setImcompleteTodo] = useState([
+    "ああああ",
+    "いいいい"
+  ]);
+  const [completeTodo, setCompleteTodo] = useState(["ううう"]);
   return (
     <>
       <div class="input-area ">
@@ -9,29 +14,30 @@ export const App = () => {
         <button>追加</button>
       </div>
       <div class="imcomplete-area">
+        <p class="title">未完了</p>
         <ul>
-          <div class="list-row">
-            <li>ああああ</li>
-            <button>完成</button>
-            <button>削除</button>
-          </div>
-          <div class="list-row">
-            <li>ああああ</li>
-            <button>完成</button>
-            <button>削除</button>
-          </div>
+          {imcompleteTodo.map((todo) => {
+            return (
+              <div key="todo" class="list-row">
+                <li>{todo}</li>
+                <button>完成</button>
+                <button>削除</button>
+              </div>
+            );
+          })}
         </ul>
       </div>
       <div class="complete-area">
+        <p class="title">完了</p>
         <ul>
-          <div class="list-row">
-            <li>うううう</li>
-            <button>戻す</button>
-          </div>
-          <div class="list-row">
-            <li>おおお</li>
-            <button>戻す</button>
-          </div>
+          {completeTodo.map((todo) => {
+            return (
+              <div key="todo" class="list-row">
+                <li>{todo}</li>
+                <button>戻す</button>
+              </div>
+            );
+          })}
         </ul>
       </div>
     </>
